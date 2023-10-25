1eec7f3d50321c56ea132047799dcd1e08bda9b8
alex <alexander.tarler@gmail.com>
2023-10-25 14:27:59 +0200

    Added a directory for transactions, and inside of that a directory for creating a transaction and one for viewing transactions. Also added a simple page for creating transactions that uploads the data to firebase database.

--- accounting/src/app/accounts/[accountID]/fiscalYears/[fiscalYearId]/page.tsx
+++ accounting/src/app/accounts/[accountID]/fiscalYears/[fiscalYearId]/page.tsx
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation";
 import { onSnapshot, doc } from "firebase/firestore";
 import { db } from "../../../../db/firebase";
 import { useState, useEffect } from "react";
+import Link from 'next/link';  // Import Link
 
 interface FiscalYear {
     id: string;
@@ -15,7 +16,9 @@ const FiscalYearPage: React.FC = () => {
     const [fiscalYear, setFiscalYear] = useState<FiscalYear | null>(null);
 
     const pathname = usePathname();
-    const fiscalYearId: any = pathname.split('/').pop();  // Updated this line
+    const pathSegments = pathname.split('/');
+    const fiscalYearId: string = pathSegments[pathSegments.length - 1]; 
+    const accountId: string = pathSegments[pathSegments.length - 3];
     
     // Fetch the fiscal year data from the database
     useEffect(() => {
@@ -40,12 +43,13 @@ const FiscalYearPage: React.FC = () => {
                 setFiscalYear(fiscalYearData);
             }
         });
+
+        
     
         return () => {
             unsubscribe();
         };
     }, [fiscalYearId]);
-    
 
     if (!fiscalYear) {
         return <div>Fiscal year not found</div>;
@@ -57,6 +61,9 @@ const FiscalYearPage: React.FC = () => {
             <p>Name: {fiscalYear.name}</p>
             <p>Start date: {fiscalYear.startDate}</p>
             <p>End date: {fiscalYear.endDate}</p>
+            <Link href={`/accounts/${accountId}/fiscalYears/${fiscalYearId}/transactions/new`}>
+                <button>Create New Transaction</button>
+            </Link>
         </div>
     );
 };
---
+++ accounting/src/app/accounts/[accountID]/fiscalYears/[fiscalYearId]/transactions/new/page.tsx
@@ -0,0 +1,80 @@
+'use client'
+import { FormEvent, useState } from "react";
+import router from "next/router";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../../../../../db/firebase";
+import { usePathname } from "next/navigation";
+
+interface Transaction {
+    description: string;
+    amount: number;
+    date: string;
+}
+
+const NewTransactionPage: React.FC = () => {
+    const [description, setDescription] = useState("");
+    const [amount, setAmount] = useState("");
+    const [date, setDate] = useState("");
+
+    const pathname = usePathname();
+    const pathSegments = pathname.split('/');
+    const fiscalYearId: string = pathSegments[pathSegments.length - 3]; 
+    const accountId: string = pathSegments[pathSegments.length - 5];
+
+    const handleCreateTransaction = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (!description || !amount || !date) {
+            console.log("All fields are required");
+            return;
+        }
+
+        const newTransaction: Transaction = {
+            description,
+            amount: parseFloat(amount),
+            date,
+        };
+
+        try {
+            await addDoc(collection(db, "transactions"), {
+                ...newTransaction,
+                accountId,
+                fiscalYearId,
+            });
+
+            
+            router.push(`/accounts/${accountId}/fiscalYears/${fiscalYearId}`);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Create New Transaction</h1>
+
+            <form onSubmit={handleCreateTransaction}>
+                <input
+                    type="text"
+                    placeholder="Description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+                <input
+                    type="number"
+                    placeholder="Amount"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                />
+                <input
+                    type="date"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                />
+                <button type="submit">Create Transaction</button>
+            </form>
+        </div>
+    );
+};
+
+export default NewTransactionPage;
