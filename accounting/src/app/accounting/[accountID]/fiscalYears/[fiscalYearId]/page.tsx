ea447d64a9c6452f5fe66af958e9908d4d853237
alex <alexander.tarler@gmail.com>
2023-11-10 16:19:16 +0100

    Made each transaction expandable in the fiscal year page

--- accounting/src/app/accounting/[accountID]/fiscalYears/[fiscalYearId]/page.tsx
+++ accounting/src/app/accounting/[accountID]/fiscalYears/[fiscalYearId]/page.tsx
@@ -4,30 +4,35 @@ import { onSnapshot, doc, collection, query, where } from "firebase/firestore";
 import { db } from "../../../../db/firebase";
 import { useState, useEffect } from "react";
 import Link from 'next/link';  
-import router from "next/router";
-
-
-interface FiscalYear {
-    id: string;
-    name: string;
-    startDate: string,
-    endDate: string
-}
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from "@mui/material/Icon";
 
 interface Transaction {
     id: string;
-    description: string;
-    amount: number;
+    entries: Entry[];
     date: string;
-}
+    fiscalYearId: string;
+  }
+  
+  interface Entry {
+    accountId: string;
+    counterAccountId: string;
+    type: 'debit' | 'credit';
+    amount: number;
+    description: string;
+  }
+  
 
 const FiscalYearPage: React.FC = () => {
-    const [fiscalYear, setFiscalYear] = useState<FiscalYear | null>(null);
+    const [fiscalYear, setFiscalYear] = useState<any>(null);
     const [transactions, setTransactions] = useState<Transaction[]>([]);
     const pathname = usePathname();
     const pathSegments = pathname.split('/');
     const fiscalYearId: string = pathSegments[pathSegments.length - 1]; 
     const accountId: string = pathSegments[pathSegments.length - 3];
+    const [expandedTransactionId, setExpandedTransactionId] = useState<string | null>(null);
     
     // Fetch the fiscal year data from the database
     useEffect(() => {
@@ -68,46 +73,33 @@ const FiscalYearPage: React.FC = () => {
         const transactionsQuery = query(
             collection(db, "transactions"),
             where("fiscalYearId", "==", fiscalYearId)
-        );
-
-        const unsubscribe = onSnapshot(transactionsQuery, (querySnapshot) => {
+          );
+        
+          const unsubscribe = onSnapshot(transactionsQuery, (querySnapshot) => {
             const fetchedTransactions: Transaction[] = [];
-            querySnapshot.forEach((doc) => {
-              const data = doc.data();
-        
-              // Check if the transaction has 'entries' (new structure)
-              if (data.entries && data.entries.length > 0) {
-                // You may want to aggregate the amounts and descriptions from the entries
-                const totalAmount = data.entries.reduce((sum: any, entry: { amount: any; }) => sum + entry.amount, 0);
-                const descriptions = data.entries.map((entry: { description: any; }) => entry.description).join(', ');
-        
-                fetchedTransactions.push({
-                  id: doc.id,
-                  description: descriptions,
-                  amount: totalAmount,
-                  date: data.date, // You may need to format this date
-                });
-              } else {
-                // Old structure
-                fetchedTransactions.push({
-                  id: doc.id,
-                  description: data.description,
-                  amount: data.amount,
-                  date: data.date, // You may need to format this date
-                });
-              }
+            querySnapshot.forEach((docSnapshot) => {
+              const data = docSnapshot.data();
+              fetchedTransactions.push({
+                id: docSnapshot.id,
+                entries: data.entries,
+                date: data.date,
+                fiscalYearId: data.fiscalYearId
+              });
             });
+            
+            // Sort transactions by date in descending order
+            fetchedTransactions.sort((a, b) => b.date.localeCompare(a.date));
+        
             setTransactions(fetchedTransactions);
           });
+        
+          return () => unsubscribe();
+        }, [fiscalYearId]);
 
-        return () => unsubscribe();
-    }, [fiscalYearId]);
 
-
-    const handleTransactionClick = (transactionId: string) => {
-        const accountId: string = pathname.split('/')[2];
-        router.push(`/accounting/${accountId}/fiscalYears/${fiscalYearId}/transactions/${transactionId}`);
-    };
+        const handleExpandClick = (transactionId: string) => {
+            setExpandedTransactionId(prev => (prev === transactionId ? null : transactionId));
+          };
 
     if (!fiscalYear) {
         return <div>Fiscal year not found</div>;
@@ -115,27 +107,38 @@ const FiscalYearPage: React.FC = () => {
 
     return (
         <div>
-            <h1>Fiscal Year</h1>
-            <p>Name: {fiscalYear.name}</p>
-            <p>Start date: {fiscalYear.startDate}</p>
-            <p>End date: {fiscalYear.endDate}</p>
-            <Link href={`/accounting/${accountId}/fiscalYears/${fiscalYearId}/transactions/new`}>
-                <button>Create New Transaction</button>
-            </Link>
-            <div className="transactions-list">
-                {transactions.map(transaction => (
-                    <div key={transaction.id} className="transaction-item">
-                        <Link 
-                            href={`/accounting/${accountId}/fiscalYears/${fiscalYearId}/transactions/${transaction.id}`} 
-                            passHref
-                        >
-                                {transaction.description} - ${transaction.amount} on {transaction.date}
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+          <h1>Fiscal Year</h1>
+          <p>Name: {fiscalYear?.name}</p>
+          <p>Start date: {fiscalYear?.startDate}</p>
+          <p>End date: {fiscalYear?.endDate}</p>
+          <Link href={`/accounting/${accountId}/fiscalYears/${fiscalYearId}/transactions/new`}>
+            <button>Create New Transaction</button>
+          </Link>
+          <div className="transactions-list">
+        {transactions.map(transaction => (
+          <Accordion 
+            key={transaction.id} 
+            expanded={expandedTransactionId === transaction.id} 
+            onChange={() => handleExpandClick(transaction.id)}
+          >
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <div>{transaction.entries[0].description} - {transaction.date}</div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div>ID: {transaction.id}</div>
+              {transaction.entries.map((entry, index) => (
+                <div key={index}>
+                  <div>{entry.type}: {entry.amount}</div>
+                  <div>Account ID: {entry.accountId}</div>
+                  <div>Counter Account ID: {entry.counterAccountId}</div>
+                </div>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FiscalYearPage;
